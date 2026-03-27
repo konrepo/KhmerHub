c7c6531b6fd7cf00d1d185c533b7a1de0c6c25e1
konrepo <khmerboxtv@gmail.com>
2026-03-27 10:23:07 -0400

    Merge KhmerDub + KhmerNuv into one service

---
+++ src/addons/KhmerDub/manifest.js
@@ -0,0 +1,38 @@
+const TYPE = "series";
+const EXTRA = ["search", "skip"];
+
+const sites = [
+  { id: "vip", name: "PhumiVip" },
+  { id: "sunday", name: "SundayDrama" },
+  { id: "phumi2", name: "PhumiClub" },
+  { id: "khmerave", name: "KhmerAve" },
+  { id: "merlkon", name: "Merlkon" },
+  { id: "idrama", name: "iDramaHD" },  
+];
+
+module.exports = {
+  id: "community.khmer.dubbed",
+  version: "3.1.0",
+  name: "KhmerDub",
+  description: "Stream Asian dramas dubbed in Khmer (Stremio App) | Dev: TheDevilz.",
+  logo: "https://avatars.githubusercontent.com/u/32822347?v=4",
+
+  resources: ["catalog", "meta", "stream"],
+  types: [TYPE],
+
+  catalogs: sites.map(site => ({
+    type: TYPE,
+    id: site.id,
+    name: site.name,
+    extraSupported: EXTRA
+  })),
+
+  behaviorHints: {
+    configurable: false
+  },
+
+  stremioAddonsConfig: {
+    issuer: "https://stremio-addons.net",
+    signature: "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..H15_k0Iyb4K2d5Gbz4-rwg.FswET_xxG8N5XtMjR6lpbNburR7DMF2Ie5NjMSlaNGneFEM-28ioA1ofdunoYFheKAmgc1t5cboQSOgTbXpjPflnSAY9DSJURdIZxfrrYg_LoOLpDqyIgOHS42t6xOYS.-gPH7tB42CWK0qMRv2HtFw"
+  }
+};
