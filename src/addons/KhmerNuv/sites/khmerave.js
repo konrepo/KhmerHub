3b0ef4f9c73be6ca9c0e5b9dac4d6f3473101335
konrepo <37514522+konrepo@users.noreply.github.com>
2026-06-18 11:52:27 -0400

    Update khmerave.js

--- src/addons/KhmerNuv/sites/khmerave.js
+++ src/addons/KhmerNuv/sites/khmerave.js
@@ -141,6 +141,7 @@ async function getEpisodes(prefix, seriesUrl) {
     return eps.map((e) => ({
       id: `${prefix}:${encodeURIComponent(seriesUrl)}:1:${e.epNumber}`,
       title: `Episode ${String(e.epNumber).padStart(2, "0")}`,
+      seriesTitle: pageTitle,
       season: 1,
       episode: e.epNumber,
       thumbnail: poster,
